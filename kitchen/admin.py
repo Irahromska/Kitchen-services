19bd955bd5e5ca280a0f5a3f872b068129e08397
Iryna Hromska <irahromska@gmail.com>
2025-09-07 23:13:44 +0300

    pdate kitchen app: models, views, admin and add urls.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import DishType, Dish, Ingredient
 
-# Register your models here.
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "dish_type", "price")
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+    filter_horizontal = ("cooks", "ingredients")
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,150 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Dish, DishType, Ingredient
+from users.models import Cook
+from .forms import (
+    DishForm,
+    DishSearchForm,
+    DishTypeSearchForm,
+    IngredientSearchForm,
+)
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "kitchen/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["num_cooks"] = Cook.objects.count()
+        context["num_dishes"] = Dish.objects.count()
+        context["num_types"] = DishType.objects.count()
+        context["num_ingredients"] = Ingredient.objects.count()
+
+        num_visits = self.request.session.get("num_visits", 0)
+        self.request.session["num_visits"] = num_visits + 1
+        context["num_visits"] = num_visits + 1
+        return context
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    paginate_by = 5
+    context_object_name = "dish_list"
+
+    def get_queryset(self):
+        queryset = Dish.objects.select_related("dish_type").prefetch_related(
+            "cooks", "ingredients"
+        )
+        name = self.request.GET.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset.order_by("name")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = DishSearchForm(
+            initial={"name": self.request.GET.get("name")}
+        )
+        return context
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    paginate_by = 5
+    context_object_name = "dishtype_list"
+
+    def get_queryset(self):
+        queryset = DishType.objects.all()
+        name = self.request.GET.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = DishTypeSearchForm(
+            initial={"name": self.request.GET.get("name")}
+        )
+        return context
+
+
+class DishTypeDetailView(LoginRequiredMixin, generic.DetailView):
+    model = DishType
+    context_object_name = "dishtype"
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = ["name"]
+    success_url = reverse_lazy("kitchen:dishtype-list")
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = ["name"]
+    success_url = reverse_lazy("kitchen:dishtype-list")
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dishtype-list")
+
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    context_object_name = "ingredient_list"
+
+    def get_queryset(self):
+        queryset = Ingredient.objects.all()
+        name = self.request.GET.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = IngredientSearchForm(
+            initial={"name": self.request.GET.get("name")}
+        )
+        return context
+
+
+class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Ingredient
+    fields = ["name"]
+    success_url = reverse_lazy("kitchen:ingredient-list")
+
+
+class IngredientUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Ingredient
+    fields = ["name"]
+    success_url = reverse_lazy("kitchen:ingredient-list")
+
+
+class IngredientDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Ingredient
+    success_url = reverse_lazy("kitchen:ingredient-list")
